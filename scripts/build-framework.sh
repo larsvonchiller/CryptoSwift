d0df86521bbdc5da7acb42bb8c35d143de15f144
Andrey Matoshko <a.matoshko@pixelplex.io>
2021-06-25 17:39:06 +0300

    Add script build-framework.sh to build phases

--- scripts/build-framework.sh
+++ scripts/build-framework.sh
@@ -51,55 +51,13 @@ mkdir -p "${OUTPUT_DIR}/iphonesimulator"
 ditto "${DERIVED_DATA_PATH}/Build/Products/Release-iphonesimulator/CryptoSwift.framework" "${OUTPUT_DIR}/iphonesimulator/CryptoSwift.framework"
 rm -rf "${DERIVED_DATA_PATH}"
 
-# tvOS
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=tvOS'
-
-mkdir -p "${OUTPUT_DIR}/appletvos"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-appletvos/CryptoSwift.framework" "${OUTPUT_DIR}/appletvos/CryptoSwift.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
-# tvOS Simulator
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=tvOS Simulator'
-
-mkdir -p "${OUTPUT_DIR}/appletvsimulator"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-appletvsimulator/CryptoSwift.framework" "${OUTPUT_DIR}/appletvsimulator/CryptoSwift.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
-# watchOS
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=watchOS'
-
-mkdir -p "${OUTPUT_DIR}/watchos"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-watchos/CryptoSwift.framework" "${OUTPUT_DIR}/watchos/CryptoSwift.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
-# watchOS Simulator
-DERIVED_DATA_PATH=$( mktemp -d )
-xcrun xcodebuild build \
-	$COMMON_SETUP \
-	-derivedDataPath "${DERIVED_DATA_PATH}" \
-	-destination 'generic/platform=watchOS Simulator'
-
-mkdir -p "${OUTPUT_DIR}/watchsimulator"
-ditto "${DERIVED_DATA_PATH}/Build/Products/Release-watchsimulator/CryptoSwift.framework" "${OUTPUT_DIR}/watchsimulator/CryptoSwift.framework"
-rm -rf "${DERIVED_DATA_PATH}"
-
 # XCFRAMEWORK
 rm -rf ${SCRIPT_DIR}/../CryptoSwift.xcframework
 xcrun xcodebuild -quiet -create-xcframework \
 	-framework "${OUTPUT_DIR}/iphoneos/CryptoSwift.framework" \
 	-framework "${OUTPUT_DIR}/iphonesimulator/CryptoSwift.framework" \
+    -framework "${OUTPUT_DIR}/macos/CryptoSwift.framework" \
+    -framework "${OUTPUT_DIR}/maccatalyst/CryptoSwift.framework" \
 	-output ${SCRIPT_DIR}/../CryptoSwift.xcframework
 
 # pushd ${OUTPUT_DIR}
